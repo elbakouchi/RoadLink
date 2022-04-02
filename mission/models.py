6d18d10c31ab85e0021d930d74e90125c4c103db
hamza <hamza.bakouchi@gmail.com>
2022-04-02 20:15:10 +0000

    save work

--- mission/models.py
+++ mission/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+from vehicle.models import Vehicle
+
+
+class Mission(models.Model):
+    vehicle = models.ForeignKey(Vehicle, default=None, on_delete=models.CASCADE, verbose_name="Véhicule")
+    beneficiary = models.CharField(verbose_name="Bénéficiaire", max_length=200)
+    division = models.ForeignKey(Di, default=None, on_delete=models.CASCADE, verbose_name="Véhicule")

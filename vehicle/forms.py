d8ca478881a2957a0957b9e416c071df129baaa0
hamza <hamza.bakouchi@gmail.com>
2022-04-03 20:03:44 +0000

    remove some useless fields

--- vehicle/forms.py
+++ vehicle/forms.py
@@ -1,7 +1,8 @@
 from django import forms
 from .models import Vehicle
 
+
 class VehicleForm(forms.ModelForm):
     class Meta:
-        model=Vehicle
-        fields = ('cost_per_km','price','registration_plate','no_of_km_travelled','mileage','vehicle_type','fuel_type','insurance_status','image')
+        model = Vehicle
+        fields = ('registration_plate', 'mileage', 'vehicle_type', 'fuel_type', 'insurance_status', 'image')
--- vehicle/models.py
+++ vehicle/models.py
@@ -18,15 +18,19 @@ class Vehicle(models.Model):
         ('B', 'En sortie'),
         ('NB', 'Dans le parc'),
         ('NB', 'En panne'),
+        ('R', 'En réparation'),
+        ('A', 'Autre'),
     )
     INSURANCE_STATUS_CHOICES = (
         ('U', 'A jour'),
-        ('NU', 'Hors écheance'),
+        ('NU', 'Hors échéance'),
     )
     VEHICLE_TYPE_CHOICES = (
         ('P', 'Utilitaire'),
         ('T', 'Luxe'),
         ('C', 'Poids lourd'),
+        ('G', 'Agricole'),
+        ('M', 'Autre'),
     )
     FUEL_TYPE_CHOICES = (
         ('D', 'Diesel'),
@@ -39,16 +43,16 @@ class Vehicle(models.Model):
     registration_plate = models.CharField("Matricule", max_length=200, default='')
     parking = models.ForeignKey(Parking, default=None, null=True, on_delete=models.CASCADE, verbose_name="Parc")
     vehicle_status = models.CharField("État du véhicule", max_length=2, default='NB', choices=VEHICLE_STATUS_CHOICES)
-    insurance_status = models.CharField("Assurance", max_length=2, default='NU', choices=INSURANCE_STATUS_CHOICES)
-    no_of_km_travelled = models.DecimalField(max_digits=20, default=0, decimal_places=0)
+    insurance_status = models.CharField("Assurance", max_length=2, default='U', choices=INSURANCE_STATUS_CHOICES)
+    # no_of_km_travelled = models.DecimalField(max_digits=20, default=0, decimal_places=0)
     fuel_type = models.CharField("Type carburant", max_length=1, default='P', choices=FUEL_TYPE_CHOICES)
-    mileage = models.DecimalField("Kilométrage", max_digits=20, default=0, decimal_places=0)
+    mileage = models.DecimalField("Kilométrage", max_digits=20, default=0, decimal_places=0, blank=True)
     vehicle_type = models.CharField("Type de véhicule", max_length=1, default='P', choices=VEHICLE_TYPE_CHOICES)
     image = models.ImageField(upload_to="vehicle_image", default="default.png", blank=True)
     # owner = models.ForeignKey(User, default=None, blank=True, verbose_name="Propriètaire")
-    cost_per_km = models.DecimalField("Consommation par kilomètre", max_digits=20, default=0, decimal_places=2,
-                                      blank=True)
-    price = models.DecimalField('Prix', max_digits=20, default="0", decimal_places=3, blank=True)
+    # cost_per_km = models.DecimalField("Consommation par kilomètre", max_digits=20, default=0, decimal_places=2,
+    #                                  blank=True)
+    # price = models.DecimalField('Prix', max_digits=20, default="0", decimal_places=3, blank=True)
     createdAt = models.DateTimeField(auto_now_add=True)
     updatedAt = models.DateTimeField(auto_now=True)
 

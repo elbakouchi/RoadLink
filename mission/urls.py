ac8bb32b3b30c24d3436f376b9f8c05a89bde32a
hamza <hamza.bakouchi@gmail.com>
2022-04-10 18:53:19 +0000

    comment usless route

--- mission/urls.py
+++ mission/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import GeneratePdf
 app_name = 'mission'
 urlpatterns = [
-    path(r'^pdf/$', GeneratePdf.as_view(), name='generatepdf'),
+    # path(r'^pdf/$', GeneratePdf.as_view(), name='generatepdf'),
 ]

c79e50cb34b4d28733633a76da2bb665b560cf44
hamza <hamza.bakouchi@gmail.com>
2022-04-10 00:49:03 +0000

    download & print actions and buttons

--- mission/admin.py
+++ mission/admin.py
@@ -1,4 +1,30 @@
+from django.urls import path
+from django.shortcuts import redirect, get_object_or_404
 from django.contrib import admin
 from .models import Mission
 
-admin.site.register(Mission)
+
+class MissionAdmin(admin.ModelAdmin):
+    list_display = ("voucher_number", "vehicle", "beneficiary",
+                    "district", "download_mission_voucher_button",
+                    "print_mission_voucher_button")
+    actions = [Mission.print_mission_voucher]
+
+    def get_urls(self):
+        urls = super().get_urls()
+        my_urls = [
+            path('print/<int:pk>/', self.print_mission_voucher, name="admin_print_mission_voucher"),
+            path('download/<int:pk>/', self.download_mission_voucher, name="admin_download_mission_voucher"),
+        ]
+        return my_urls + urls
+
+    def download_mission_voucher(self, request, pk):
+        mission = get_object_or_404(Mission, pk=pk)
+        return mission.print_mission_voucher('attachment')
+
+    def print_mission_voucher(self, request, pk):
+        mission = get_object_or_404(Mission, pk=pk)
+        return mission.print_mission_voucher('inline')
+
+
+admin.site.register(Mission, MissionAdmin)

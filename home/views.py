a30b65bfa058fec0e44d0bb9a15899e4f6e4d8a9
hamza <hamza.bakouchi@gmail.com>
2022-04-10 00:54:45 +0000

    reformat

--- home/views.py
+++ home/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.http import Http404
 from django.template import loader
 
+
 # Create your views here.
 def index(request):
-    return render(request,'home/index.html')
+    return render(request, 'home/index.html')
+
 
 def notfound(request):
-	return render(request,'home/404.html')
+    return render(request, 'home/404.html')

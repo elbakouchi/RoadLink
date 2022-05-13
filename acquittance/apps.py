859ca4b54f962c05b423a86ac645b1776d68d379
hamza <hamza.bakouchi@gmail.com>
2022-05-14 00:16:16 +0100

    fix typo

--- acquittance/apps.py
+++ acquittance/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class AcquittanceConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'acquittance'
+    verbose_name = "Décharges"
--- organism/apps.py
+++ organism/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class OrganismConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'organism'
-    verbose_name = "Organismes et Services"
+    verbose_name = "Organismes et Affecations"
